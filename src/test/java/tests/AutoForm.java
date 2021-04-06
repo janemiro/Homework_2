23f219b9fc620e27c688d0d33f48186cd8c73838
Evgeniia Miroshina <evgeniia.miroshina@t-systems.com>
2021-04-07 00:31:18 +0300

    corrected Homework

--- src/test/java/tests/AutoForm.java
+++ src/test/java/tests/AutoForm.java
@@ -3,10 +3,8 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
